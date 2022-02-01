861968e4f6cf0cca1b6dc08e0a61002a7ac90fab
Jeong Jihoon <binoopang@gmail.com>
2022-02-01 17:28:41 +0900

    아이들이 노는틈을 타 occur* 추가!

--- chap5.rkt
+++ chap5.rkt
@@ -29,9 +29,29 @@
       )
     )
   )
+
+(define add1
+  (lambda (x)
+    (+ x 1)
+    )
+  )
+
+(define occur*
+  (lambda (a l)
+    (cond
+      ((null? l) 0)
+      ((atom? (car l))
+       (cond
+         ((eq? a (car l)) (add1 (occur* a (cdr l))))
+         (else (occur* a (cdr l)))))
+      (else (+ (occur* a (car l)) (occur* a (cdr l))))
+      )
+    )
+  )
             
 
 (check-expect (rember* 'a '(a b c d a b a)) '(b c d b))
 (check-expect (rember* 'a '(a (b a d) c d a b a)) '((b d) c d b))
 (check-expect (insertR* 'z 'a '(a b (b a c) d)) '(a z b (b a z c) d))
+(check-expect (occur* 'a '(a b c d)) 1)
 (test)
